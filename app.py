e95ba8200d9fb1ed008f89959fb01c4f5fab5c3a
Giancarlo Perrone <gperrone@westhealth.org>
2016-12-19 21:03:28 -0800

    index

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify
+from flasgger import Swagger
 import scraper
 import os
+import connexion
 
-app = Flask(__name__)
 
+app = Flask(__name__, static_url_path="")
+Swagger(app)
 
 """
 ===================
@@ -39,7 +42,7 @@ ow_ps4 = "https://playoverwatch.com/en-us/career/psn/"
 
 @app.route('/')
 def index():
-	return "Index Page"
+	return app.send_static_file('index.html')
 
 
 """
@@ -47,15 +50,50 @@ def index():
 Xbox methods
 ============
 """
-@app.route('/<string:btag>/quickplay/xbl/', methods = ['GET'])
-def get_xbox_quickplay(btag):
-	resp = scraper.get_qp_hero_data(btag, "quickplay", ow_xbox)
+
+@app.route('/<string:btag>/<string:mode>/xbl/', methods = ['GET'])
+def get_xbox_data(btag, mode):
+	"""
+        Get a JSON representation of all your hero's stats.
+        ---
+        tags:
+          - Xbox
+        parameters:
+          - name: btag
+            in: path
+            description: Xbox gamertag
+            required: true
+            type: string
+            default: none
+          - name: mode
+            in: path
+            description: request competetive or quickplay stats. (Accepted parameters are "quickplay" or "competetive")
+            required: true
+            type: string
+            default: none
+        responses:
+          200:
+            description: Returns all heroes stats.
+            schema:
+                type: object
+                items:
+                    $ref: '#/definitions/Xbox'
+        """
+	resp = scraper.get_qp_hero_data(btag, mode, ow_xbox)
 	return jsonify(resp)
 
-@app.route('/<string:btag>/competetive/xbl/', methods = ['GET'])
-def get_xbox_competetive(btag):
-	resp = scraper.get_qp_hero_data(btag, "competetive", ow_xbox)
-	return jsonify(resp)
+# @app.route('/<string:btag>/quickplay/xbl/', methods = ['GET'])
+# def get_xbox_quickplay(btag):
+# 	"""
+
+# 	"""
+# 	resp = scraper.get_qp_hero_data(btag, "quickplay", ow_xbox)
+# 	return jsonify(resp)
+
+# @app.route('/<string:btag>/competetive/xbl/', methods = ['GET'])
+# def get_xbox_competetive(btag):
+# 	resp = scraper.get_qp_hero_data(btag, "competetive", ow_xbox)
+# 	return jsonify(resp)
 
 
 
@@ -64,15 +102,21 @@ def get_xbox_competetive(btag):
 Ps4 methods
 ===========
 """
-@app.route('/<string:btag>/quickplay/psn/', methods = ['GET'])
-def get_ps4_quickplay(btag):
-	resp = scraper.get_qp_hero_data(btag, "quickplay", ow_ps4)
+@app.route('/<string:btag>/<string:mode>/psn/', methods = ['GET'])
+def get_ps4_data(btag, mode):
+	resp = scraper.get_qp_hero_data(btag, mode, ow_ps4)
 	return jsonify(resp)
 
-@app.route('/<string:btag>/competetive/psn/', methods = ['GET'])
-def get_ps4_competetive(btag):
-	resp = scraper.get_qp_hero_data(btag, "competetive", ow_ps4)
-	return jsonify(resp)
+
+# @app.route('/<string:btag>/quickplay/psn/', methods = ['GET'])
+# def get_ps4_quickplay(btag):
+# 	resp = scraper.get_qp_hero_data(btag, "quickplay", ow_ps4)
+# 	return jsonify(resp)
+
+# @app.route('/<string:btag>/competetive/psn/', methods = ['GET'])
+# def get_ps4_competetive(btag):
+# 	resp = scraper.get_qp_hero_data(btag, "competetive", ow_ps4)
+# 	return jsonify(resp)
 
 
 
@@ -81,16 +125,22 @@ def get_ps4_competetive(btag):
 Korean server methods
 =====================
 """
-@app.route('/<string:btag>/quickplay/pc/kr/', methods = ['GET'])
-def get_pc_quickplay_korea(btag):
-	resp = scraper.get_qp_hero_data(btag, "quickplay", ow_kr_pc)
+@app.route('/<string:btag>/<string:mode>/pc/kr', methods = ['GET'])
+def get_pc_kr_data(btag, mode):
+	resp = scraper.get_qp_hero_data(btag, mode, ow_kr_pc)
 	return jsonify(resp)
+
+
+# @app.route('/<string:btag>/quickplay/pc/kr/', methods = ['GET'])
+# def get_pc_quickplay_korea(btag):
+# 	resp = scraper.get_qp_hero_data(btag, "quickplay", ow_kr_pc)
+# 	return jsonify(resp)
 	
 
-@app.route('/<string:btag>/competetive/pc/kr/', methods = ['GET'])
-def get_pc_competetive_korea(btag):
-	resp = scraper.get_qp_hero_data(btag, "competetive", ow_kr_pc)
-	return jsonify(resp)
+# @app.route('/<string:btag>/competetive/pc/kr/', methods = ['GET'])
+# def get_pc_competetive_korea(btag):
+# 	resp = scraper.get_qp_hero_data(btag, "competetive", ow_kr_pc)
+# 	return jsonify(resp)
 
 
 """
@@ -98,16 +148,21 @@ def get_pc_competetive_korea(btag):
 Europe server methods
 =====================
 """
-@app.route('/<string:btag>/quickplay/pc/eu/', methods = ['GET'])
-def get_pc_quickplay_europe(btag):
-	resp = scraper.get_qp_hero_data(btag, "quickplay", ow_eu_pc)
+@app.route('/<string:btag>/<string:mode>/pc/eu/', methods = ['GET'])
+def get_pc_eu_data(btag, mode):
+	resp = scraper.get_qp_hero_data(btag, mode, ow_eu_pc)
 	return jsonify(resp)
+
+# @app.route('/<string:btag>/quickplay/pc/eu/', methods = ['GET'])
+# def get_pc_quickplay_europe(btag):
+# 	resp = scraper.get_qp_hero_data(btag, "quickplay", ow_eu_pc)
+# 	return jsonify(resp)
 	
 
-@app.route('/<string:btag>/competetive/pc/eu/', methods = ['GET'])
-def get_pc_competetive_europe(btag):
-	resp = scraper.get_qp_hero_data(btag, "competetive", ow_eu_pc)
-	return jsonify(resp)
+# @app.route('/<string:btag>/competetive/pc/eu/', methods = ['GET'])
+# def get_pc_competetive_europe(btag):
+# 	resp = scraper.get_qp_hero_data(btag, "competetive", ow_eu_pc)
+# 	return jsonify(resp)
 	
 
 """
@@ -115,17 +170,22 @@ def get_pc_competetive_europe(btag):
 America server methods
 ======================
 """
-@app.route('/<string:btag>/quickplay/pc/us/', methods = ['GET'])
-def get_pc_quickplay_usa(btag):
-	# print btag
-	resp = scraper.get_qp_hero_data(btag, "quickplay", ow_us_pc)
-	# print btag
+@app.route('/<string:btag>/<string:mode>/pc/us/', methods = ['GET'])
+def get_pc_us_data(btag, mode):
+	resp = scraper.get_qp_hero_data(btag, mode, ow_us_pc)
 	return jsonify(resp)
 
-@app.route('/<string:btag>/competetive/pc/us/', methods = ['GET'])
-def get_pc_competetive_usa(btag):
-	resp = scraper.get_qp_hero_data(btag, "competetive", ow_us_pc)
-	return jsonify(resp)
+# @app.route('/<string:btag>/quickplay/pc/us/', methods = ['GET'])
+# def get_pc_quickplay_usa(btag):
+# 	# print btag
+# 	resp = scraper.get_qp_hero_data(btag, "quickplay", ow_us_pc)
+# 	# print btag
+# 	return jsonify(resp)
+
+# @app.route('/<string:btag>/competetive/pc/us/', methods = ['GET'])
+# def get_pc_competetive_usa(btag):
+# 	resp = scraper.get_qp_hero_data(btag, "competetive", ow_us_pc)
+# 	return jsonify(resp)
 
 
 @app.route('/hello', methods = ['GET'])
@@ -133,6 +193,14 @@ def test():
 	return "hello"
 
 
+
+# for swagger
+# if __name__ == '__main__':
+#     app = connexion.App(__name__, specification_dir='./dist/')
+#     app.add_api('swagger.yaml', arguments={'title': 'Move your app forward with the Overwatch API'})
+#     app.run(port=8080)
+
+# without swagger
 if __name__== '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
--- get_data.py
+++ get_data.py
@@ -2,68 +2,76 @@ import urllib2
 import json
 import requests
 import pandas as pd
+import scraper
 
 debug = 0
+
+api_endpoint = "https://afternoon-dusk-72724.herokuapp.com/"
 
 def get_info():
 	valid_sys = ['pc', 'xbl', 'psn']
 	valid_regs = ['us', 'eu', 'kr', 'cn', 'global']
-	valid_heroes = ["Genji",
-					"McCree",
-					"Pharah",
-					"Reaper",
-					"Soldier 76",
-					"Tracer",
-					"Bastion",
-					"Hanzo",
-					"Junkrat",
-					"Mei",
-					"Torbjorn",
-					"Widowmaker",
-					"D.va",
-					"Reinhardt",
-					"Roadhog",
-					"Winston",
-					"Zarya",
-					"Ana",
-					"Lucio",
-					"Mercy",
-					"Symmetra",
-					"Zenyatta"]
+	
+	valid_heroes = scraper.get_possible_heros()
+
+
 	flag = 1
 	while flag:
-		sys = raw_input("Please enter your system: ")
+		sys = raw_input("Please enter your system:\n" + str(valid_sys) + "\n")
 		if sys in valid_sys:
 			break
 		else:
 			print "Please enter 'pc', 'xbl', or 'psn'."
 	while flag:
-		reg = raw_input("Please enter your region: ")
+		reg = raw_input("Please enter your region:\n" + str(valid_regs) + "\n")
 		if reg in valid_regs:
 			break
 		else:
 			print "Please enter 'us', 'eu', 'kr', 'cn', or 'global'"
 	while flag:
-		hero = raw_input("What hero do you want data for? ")
+		hero = raw_input("What hero do you want data for?\n" + str(valid_heroes) + "\n")
 		if hero in valid_heroes:
 			break
 		else:
 			print "Please enter a valid hero name. Options are: \n"
 			print valid_heroes
+	while flag:
+		mode = raw_input("quickplay or competetive? Enter [qp] or [comp]" + "\n")
+			
+		if mode == "qp":
+			mode = "quickplay"
+			break
+		elif mode == "comp":
+			mode = "competetive"
+			break
+		else:
+			print "Please enter [qp] or [comp]"
+
+
 	battle_tag = raw_input("Enter battle_tag: ")
 
-	return sys, reg, battle_tag, hero
+	return sys, reg, battle_tag, mode, hero
 
 if debug:
 	url = "https://api.lootbox.eu/pc/us/boludo00-1183/quick-play/hero/Genji/"
 else:
-	sys, reg, battle_tag, hero = get_info()
-	url = "https://api.lootbox.eu/" + sys + "/" + reg + "/" + battle_tag + "/quick-play/hero/" + hero +"/"
+	sys, reg, battle_tag, mode, hero = get_info()
+	if(sys == "pc"):
+		url = api_endpoint + battle_tag + "/" + mode + "/" + sys + "/" + reg + "/"
+	else:
+		url = api_endpoint + battle_tag + "/" + mode + "/" + sys + "/"
 
-print "Getting request..."
+print "Getting request for " + hero + "..."
 request = requests.get(url=url)
-data = json.loads(request.text)
 
-df = pd.DataFrame(data)
+json_req = request.json()
 
-print df
+print json.dumps(json_req[hero], indent = 4)
+
+# print request.text
+
+# data = json.loads(request.text)
+
+# df = pd.DataFrame(data)
+
+# print df
